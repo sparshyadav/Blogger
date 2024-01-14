308b6973b26eb300fe062403db6bb020d4d8f529
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-14 14:55:48 +0530

    New Routes Added

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { signup } = require("../contollers/authController");
+const { addBlog, showAllBlogs } = require("../contollers/blogController");
 
 router.post("/addBlog", addBlog);
 router.get("/showAllBlogs", showAllBlogs);
+
+router.post("/signup", signup);
