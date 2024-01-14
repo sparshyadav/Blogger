8217f5210253c3cf51617756250616b9ef619c09
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-14 14:36:41 +0530

    Show Blogs Controller Created

--- contollers/blogController.js
+++ contollers/blogController.js
@@ -1,4 +1,4 @@
-const Blogs = require("../models/blog");
+const Blog = require("../models/blog");
 const User = require("../models/user");
 
 exports.addBlog = async (req, res) => {
@@ -20,3 +20,43 @@ exports.addBlog = async (req, res) => {
     });
   }
 };
+
+exports.showAllBlogs = async (req, res) => {
+  try {
+    const allBlogs = await Blog.find({ isVerified: true });
+    res.status(200).json({
+      success: true,
+      data: allBlogs,
+      message: "All Blogs Shown"
+    })
+  }
+  catch (err) {
+    console.error(err);
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      data: "Error Occured while Showing Blogs",
+    });
+  }
+}
+
+exports.adminBlogs = async (req, res) => {
+  try {
+    const nonVerifiedBlogs = await Blog.find({ isVerified: false });
+    res.status(200).json({
+      success: true,
+      data: nonVerifiedBlogs,
+      message: "All Blogs Shown that Admin has to Verify"
+    })
+  }
+  catch (err) {
+    console.error(err);
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      data: "Error Occured while Showing Blogs that Admin has to Verify",
+    });
+  }
+}
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,4 +2,4 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/addBlog", addBlog);
-
+router.get("/showAllBlogs", showAllBlogs);
