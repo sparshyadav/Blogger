777416e8b013e079e320f00c936ee1759f7f4638
t-sweta <swetashreya004@gmail.com>
2024-01-14 13:55:00 +0530

    Isverified Parameter Added to Schema

--- models/blog.js
+++ models/blog.js
@@ -1,19 +1,23 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const blogSchema=new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    },
-    title: {
-        type: String,
-        required: true,
-        maxLength: 10
-    },
-    content: {
-        type: String,
-        required: true,
-    }
-})
+const blogSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+    maxLength: 10,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-module.exports=mongoose.model("Blog", blogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
