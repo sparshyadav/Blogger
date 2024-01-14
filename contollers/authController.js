f5fcf89b50a04e9d4a44cab214add02766ebbf7d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-01-14 15:31:42 +0530

    Formatted and Updated Code

--- contollers/authController.js
+++ contollers/authController.js
@@ -40,61 +40,64 @@ exports.signup = async (req, res) => {
             message: "User Cannot be Registered, Please Try Again later"
         })
     }
-  }
-exports.login = async (req,res) => {
-  try{
-    //data fetch
-    const {email,password} = req.body;
-    if(!email || !password){
-      return res.status(400).json({
-        success:false,
-        message:'Please fill all the details carefully'
-      });
-    }
-    let user = await User.findOne({email});
-    if(!user){
-      return res.status(401).json({
-        success:false,
-        message:'User is not registered',
-      });
-    }
-    const payload = {
-      email : user.email,
-      id:user._id, 
-      role:user.role   
-    };
-    if(await bcrypt.compare(password,user.password)){
-      let token = jwt.sign(payload,
-        process.env.JWT_SECRET,
-        {
-          expiresIn:"2h",
-        });
-        user = user.toObject();
-        user.token = token;
-        user.password = undefined;
-        const options = {
-          expires : new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
-          httpOnly:true,
+}
+
+exports.login = async (req, res) => {
+    try {
+        //data fetch
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: 'Please fill all the details carefully'
+            });
         }
-        res.cookie("blogCookie",token,options).status(200).json({
-          success:true,
-          token,
-          user,
-          message:'User logged in Successfully',
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: 'User is not registered',
+            });
+        }
+
+        const payload = {
+            email: user.email,
+            id: user._id,
+            role: user.role
+        };
+        if (await bcrypt.compare(password, user.password)) {
+            let token = jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "2h",
+                });
+            user = user.toObject();
+            user.token = token;
+            user.password = undefined;
+            const options = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+            }
+            res.cookie("blogCookie", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message: 'User logged in Successfully',
+            });
+        }
+        else {
+            return res.status(403).json({
+                success: false,
+                message: "Password Incorrect"
+            });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            message: "Login failure",
         });
     }
-    else{
-      return res.status(403).json({
-        success:false,
-        message:"Password Incorrect"
-      });
-    }
-  }
-  catch(err){
-    console.log(err);
-    return res.status(500).json({
-      success:false,
-      message:"Login failure",
-    });
-  }
 }
